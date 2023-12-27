d2d49152294fee9cbbf76391f9b29ba3198a9c34
fawasam <fawasam5@gmail.com>
2023-12-27 10:20:13 +0530

    pagenot found designed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import { lookInSession } from "./common/session";
 import Editor from "./pages/editor.pages";
 import HomePage from "./pages/home.page";
 import SearchPage from "./pages/search.page.jsx";
+import PageNotFound from "./pages/404.page.jsx";
 
 export const UserContext = createContext({});
 const App = () => {
@@ -29,6 +30,7 @@ const App = () => {
           <Route path="signin" element={<UserAuthForm type="sign-in" />} />
           <Route path="signup" element={<UserAuthForm type="sign-up" />} />
           <Route path="search/:query" element={<SearchPage />} />
+          <Route path="*" element={<PageNotFound />} />
         </Route>
       </Routes>
     </UserContext.Provider>
